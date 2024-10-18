5775da314a095ea2ed030a8a1c9289dab0672a11
aidanmascoli <aidantm717@gmail.com>
2024-10-18 18:04:55 -0400

    some frontend stuff haha teehee

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,31 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    theme: [
+      {
+        'spotify-dark': {
+          'base-100': '#100B12',
+          'base-200': '#2C272E',
+          'base-300': '#47404A',
+          'neutral': '#A39CA6',
+          'accent': '#53DD6C',
+          'primary': '#753188',
+          'secondary': '#E59934',
+        }
+      },
+      {
+        'spotify-light': {
+          'base-100': '#FBFAFC',
+          'base-200': '#F0EDF2',
+          'base-300': '#E1DEE3',
+          'neutral': '#BEB8C2',
+          'accent': '#53DD6C',
+          'primary': '#AF53C9',
+          'secondary': '#F8AE4C',
+        }
+      }
+    ]
+  },
   plugins: [daisyui],
 }
