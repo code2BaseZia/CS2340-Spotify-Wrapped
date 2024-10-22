1a573a93b36595f73961820266b1df4a7ec40999
Matthew Nehme-Haily <matthewnehme04@gmail.com>
2024-10-22 01:45:42 -0400

    spotify api authentication (hip hip hooray!) + (tim my goat fr fr)

--- api/urls.py
+++ api/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from wrapped.views import WrappedRedirectView
+
+from django.urls import path
+from .views import AuthURL
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('wrapped/', include("wrapped.urls")),
-    path('api/', include("api.urls")),
-    path('', WrappedRedirectView.as_view()),
+
+    path('auth', AuthURL.as_view()),
 ]
---
+++ api/views.py
@@ -0,0 +1,41 @@
+import os
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from requests import Request, post
+from django.urls import reverse_lazy
+
+
+
+# Create your views here.
+class AuthURL(APIView):
+    def get(self, request, format=None):
+        scopes = 'user-top-read user-read-recently-played user-library-read user-follow-read playlist-read-private playlist-read-collaborative'
+
+        url = Request('GET', 'https://accounts.spotify.com/authorize', params={
+            'scope': scopes,
+            'response_type': 'code',
+            'redirect_uri': reverse_lazy("home"),
+            'client_id': os.getenv('SPOTIPY_CLIENT_ID')
+        }).prepare().url
+
+        return Response({'url': url}, status=status.HTTP_200_OK)
+
+def spotify_callback(request, format=None):
+    code = request.GET.get('code')
+    error = request.GET.get('error')
+
+    response = post('https://accounts.spotify.com/api/token', data={
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': reverse_lazy("home"),
+        'client_id': os.getenv('SPOTIPY_CLIENT_ID'),
+        'client_secret': os.getenv('SPOTIPY_CLIENT_SECRET')
+    }).json()
+
+    access_token = response.get('access_token')
+    token_type = response.get('token_type')
+    refresh_token = response.get('refresh_token')
+    expires_in = response.get('expires_in')
+    error = response.get('error')
