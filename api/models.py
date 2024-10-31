3dd3515cd661cd7ea9b486b9e4f5c4b14d896a8a
code2BaseZia <147295900+code2BaseZia@users.noreply.github.com>
2024-10-31 15:14:23 -0400

    Added SpotifyUserWrap model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from wrapped.models import Profile
 
 
 class SpotifyToken(models.Model):
@@ -8,3 +9,18 @@ class SpotifyToken(models.Model):
     access_token = models.CharField(max_length=150)
     expires_in = models.DateTimeField()
     token_type = models.CharField(max_length=50)
+
+
+class SpotifyUserWrap(models.Model):
+    THEME = {
+        "no": "None",
+        "ch": "Christmas",
+        "hw": "Halloween",
+        "ea": "Easter",
+    }
+
+    wrapped_id = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    theme = models.CharField(default="no", max_length=2, choices=THEME.items())
+    # When the user who generated these wraps deletes their account, the on_delete will delete their wraps
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
