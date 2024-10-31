3dccd2b9d036d33acaeedde8b632a03c16e01f49
code2BaseZia <147295900+code2BaseZia@users.noreply.github.com>
2024-10-31 15:22:12 -0400

    Fixed circular import by refactor

--- api/models.py
+++ api/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from wrapped.models import Profile
 
 
 class SpotifyToken(models.Model):
@@ -9,18 +8,3 @@ class SpotifyToken(models.Model):
     access_token = models.CharField(max_length=150)
     expires_in = models.DateTimeField()
     token_type = models.CharField(max_length=50)
-
-
-class SpotifyUserWrap(models.Model):
-    THEME = {
-        "no": "None",
-        "ch": "Christmas",
-        "hw": "Halloween",
-        "ea": "Easter",
-    }
-
-    wrapped_id = models.CharField(max_length=50, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    theme = models.CharField(default="no", max_length=2, choices=THEME.items())
-    # When the user who generated these wraps deletes their account, the on_delete will delete their wraps
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
